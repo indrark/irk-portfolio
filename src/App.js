aabca7237b4243dff5e5e20f4cd6f2a358263a35
Indraneel Kulkarni <16199753+indrark@users.noreply.github.com>
2020-05-05 21:22:23 -0400

    Prod Build
    published website to indraneelkulkarni.com

--- src/App.js
+++ src/App.js
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This is the beginning of my portfolio.
+         Indraneel Kulkarni
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
